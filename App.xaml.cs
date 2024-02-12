b443bd3ba0cbbbbba804edc2d028c48b21d40a72
m4d1am <149973370+m4d1am@users.noreply.github.com>
2024-02-12 23:03:37 +0100

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace Budget_Planner_Alnur_Madiyev_w68646
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+
+    }
+}
---
+++ Main_Window.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Budget_Planner_Alnur_Madiyev_w68646
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) //all window to be moved even with window styling set to none 
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+
+
+
+        public void Btn_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0); //when close button clicked exit program
+        }
+
+        public void Rd_Budget_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+
+        public void Rd_Home_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void Cb_Dark_Checked(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
